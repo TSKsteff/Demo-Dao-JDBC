6c1ace0290d0b00086844487cff0a8d01a0e8505
TSKsteff <steff@edu.univali.br>
2023-09-30 13:40:11 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,18 @@
 package application;
 
+import java.util.Date;
+
+import model.entities.Deparment;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Deparment obj = new Deparment(1,"Books");
+		
+		Seller seller =  new Seller(21,"bob", "bob@gmail.com", new Date(), 3000.00,obj);
+		System.out.println(obj);
+		
 	}
 
 }
