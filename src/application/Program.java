0bc731bcb47ce5b47289dbd167fbb20eae205b58
TSKsteff <steff@edu.univali.br>
2023-09-30 22:44:37 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,7 +31,16 @@ public class Program {
 		
 		for (Seller obj : list) {
 			System.out.println(obj);
+		
 		}
+		
+		System.out.println("========\tTeste 4: seller insert\t=======");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Insered! Id: "+newSeller.getId());
+		
 	}
+	
+	
 
 }
